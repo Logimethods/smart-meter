f38f68ca37c7ece60d34d074cdd0e9c191cf571b
Laurent Magnin <laurent.magnin@logimethods.com>
2016-11-03 12:29:42 -0400

    stop.sh COMPLETED

--- stop.sh
+++ stop.sh
@@ -1,2 +1,6 @@
 docker service rm $(docker service ls -q)
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+#https://gist.github.com/brianclements/f72b2de8e307c7b56689
+docker rmi $(docker images | grep "<none>" | awk '{print $3}') 2>/dev/null || echo "No untagged images to delete."
 
