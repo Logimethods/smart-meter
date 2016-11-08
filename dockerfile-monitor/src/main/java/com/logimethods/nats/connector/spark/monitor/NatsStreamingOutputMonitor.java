4bdcb94deac97705e8a676bd9a2b394a95e9d36e
Laurent Magnin <laurent.magnin@logimethods.com>
2016-11-08 16:04:58 -0500

    [ci skip] MIT Licence ADDED

--- dockerfile-monitor/src/main/java/com/logimethods/nats/connector/spark/monitor/NatsStreamingOutputMonitor.java
+++ dockerfile-monitor/src/main/java/com/logimethods/nats/connector/spark/monitor/NatsStreamingOutputMonitor.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Logimethods
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License (MIT)
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *******************************************************************************/
+
 package com.logimethods.nats.connector.spark.monitor;
 
 import java.io.IOException;
