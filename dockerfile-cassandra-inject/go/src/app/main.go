33412fbb491a7bdb7aa37bbb11636fea984c78b9
Laurent Magnin <laurent.magnin@logimethods.com>
2016-11-14 16:53:08 -0500

    nats.Connect(nats.DefaultURL)

--- dockerfile-cassandra-inject/go/src/app/main.go
+++ dockerfile-cassandra-inject/go/src/app/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"time"
+	
+	"github.com/nats-io/nats"
 )
 
 func main() {
 	fmt.Println("Welcome to the playground!")
 
 	fmt.Println("The time is", time.Now())
+	
+	nc, _ := nats.Connect(nats.DefaultURL)
+	
+	// Simple Async Subscriber
+	nc.Subscribe("foo", func(m *nats.Msg) {
+	    fmt.Printf("Received a message: %s\n", string(m.Data))
+	})
 }
-
