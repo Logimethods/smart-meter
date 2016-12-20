adcb7b626040d4a1efe1d1687c3cc28ea9ebe3e4
Laurent Magnin <laurent.magnin@logimethods.com>
2016-12-20 09:41:06 -0500

    Ports of Cassandra exposed

--- cassandra-populate.sh
+++ cassandra-populate.sh
@@ -16,6 +16,8 @@ docker service create \
 	--mount type=volume,source=cassandra-volume,destination=/var/lib/cassandra \
 	-e CASSANDRA_BROADCAST_ADDRESS="cassandra-root" \
 	-e CASSANDRA_CLUSTER_NAME="Smartmeter Cluster" \
+	-p 9042:9042 \
+	-p 9160:9160 \
 	logimethods/smart-meter:cassandra$1
 		
 docker service create \
