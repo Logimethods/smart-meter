c5144fcdfe4176d33667a97164e3e7a731c3a725
Laurent Magnin <laurent.magnin@logimethods.com>
2017-02-13 17:34:38 -0500

    Cassandra Cluster (tentative)
    WARNING: The Docker Engine you're using is running in swarm mode.
    
    Compose does not use swarm mode to deploy services to multiple nodes in
    a swarm. All containers will be scheduled on the current node.
    
    To deploy your application across the swarm, use `docker stack deploy`.

--- cqlsh.sh
+++ cqlsh.sh
@@ -1,2 +1,4 @@
-docker exec -it $(docker ps | grep "cassandra" | rev | cut -d' ' -f1 | rev) cqlsh 
+# docker exec -it $(docker ps | grep "cassandra" | rev | cut -d' ' -f1 | rev) cqlsh
 						# $(docker ps | grep "cassandra" | rev | cut -d' ' -f1 | rev)
+
+docker run -it --rm --net=smart-meter-net logimethods/smart-meter:cassandra sh -c 'exec cqlsh "cassandra-1"'
