4623e47a19591f439a0e1e3c64d6bcb1e83763fb
Laurent Magnin <laurent.magnin@logimethods.com>
2017-02-22 13:36:30 -0500

    cfstats smartmeter.[*]

--- dockerfile-telegraf/script/cfstats.sh
+++ dockerfile-telegraf/script/cfstats.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-/nodetool/dsc-cassandra-3.0.9/bin/nodetool -h cassandra_main -u cassandra -pw ${JMX_PASSWORD} cfstats smartmeter.raw_voltage_data  | grep "Local write count" | rev | cut -d ' ' -f 1 | rev
+# /nodetool/dsc-cassandra-3.0.9/bin/nodetool -h cassandra_main -u cassandra -pw ${JMX_PASSWORD} cfstats smartmeter.raw_voltage_data  | grep "Local write count" | rev | cut -d ' ' -f 1 | rev
+
+values=$( /nodetool/dsc-cassandra-3.0.9/bin/nodetool -h cassandra_main -u cassandra -pw ${JMX_PASSWORD} cfstats smartmeter  | grep "Local write count" | rev | cut -d ' ' -f 1 | rev )
+
+total=0
+IFS=$'\n'
+for value in $values
+do
+  total=$(( total + value ))
+done
+echo "$total"
