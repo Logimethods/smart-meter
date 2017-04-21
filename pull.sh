0d22be16db48b31c8984123ce285b8be51b1dbf9
Laurent Magnin <laurent.magnin@logimethods.com>
2017-04-21 08:46:48 -0400

    [ci skip] pull completed

--- pull.sh
+++ pull.sh
@@ -1,3 +1,8 @@
+docker pull logimethods/smart-meter:telegraf
+docker pull logimethods/smart-meter:cassandra-inject
+docker pull logimethods/smart-meter:cassandra
 docker pull logimethods/smart-meter:inject
 docker pull logimethods/smart-meter:monitor
+docker pull logimethods/smart-meter:nats-server
+docker pull logimethods/smart-meter:app-batch
 docker pull logimethods/smart-meter:app-streaming
