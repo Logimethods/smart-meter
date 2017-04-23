de9b83c15cda84c45b1e121a303a1af668887d62
Laurent Magnin <laurent.magnin@logimethods.com>
2017-04-23 15:50:43 -0400

    | head -4 | tail -1

--- dockerfile-telegraf/script/cassandra_count.sh
+++ dockerfile-telegraf/script/cassandra_count.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #renice 19 -p $$
-docker exec $(docker ps | grep $CASSANDRA_URL | awk '{ print $1 }') cqlsh -e "select count(*) from ${TELEGRAF_CASSANDRA_TABLE};"
+docker exec $(docker ps | grep $CASSANDRA_URL | awk '{ print $1 }') cqlsh -e "select count(*) from ${TELEGRAF_CASSANDRA_TABLE};" | head -4 | tail -1
