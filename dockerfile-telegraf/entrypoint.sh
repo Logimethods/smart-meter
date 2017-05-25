b4dd298c04da84e0b65f7d2a30e99d7dd0317c37
Laurent Magnin <laurent.magnin@logimethods.com>
2017-05-25 11:26:23 -0400

    http://registry:5000/service/NAME/

--- dockerfile-telegraf/entrypoint.sh
+++ dockerfile-telegraf/entrypoint.sh
@@ -8,7 +8,7 @@ if [ -z "$NODE_ID" ]
 then
     export DOCKER_TARGET_ID=$(wget -q -O - http://registry:5000/container/name/${DOCKER_TARGET_NAME}.)
 else
-    export DOCKER_TARGET_ID=$(wget -q -O - http://registry:5000/service/id/${NODE_ID}/${DOCKER_TARGET_NAME})
+    export DOCKER_TARGET_ID=$(wget -q -O - http://registry:5000/service/name/${NODE_ID}/${DOCKER_TARGET_NAME})
 fi
 
 echo "NODE_ID: ${NODE_ID}"
