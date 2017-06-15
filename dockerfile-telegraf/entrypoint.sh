98817c3050b81179078500ea16cbd3e896712d1e
Laurent Magnin <laurent.magnin@logimethods.com>
2017-06-15 13:35:26 -0400

    Telegraf: http://eureka:5000/service/name/

--- dockerfile-telegraf/entrypoint.sh
+++ dockerfile-telegraf/entrypoint.sh
@@ -6,9 +6,9 @@ set -e
 #
 if [ -z "$NODE_ID" ]
 then
-    export DOCKER_TARGET_ID=$(wget -q -O - http://registry:5000/container/name/${DOCKER_TARGET_NAME})
+    export DOCKER_TARGET_ID=$(wget -q -O - http://eureka:5000/container/name/${DOCKER_TARGET_NAME})
 else
-    export DOCKER_TARGET_ID=$(wget -q -O - http://registry:5000/service/name/${NODE_ID}/${DOCKER_TARGET_NAME})
+    export DOCKER_TARGET_ID=$(wget -q -O - http://eureka:5000/service/name/${NODE_ID}/${DOCKER_TARGET_NAME})
 fi
 
 echo "NODE_ID: ${NODE_ID}"
