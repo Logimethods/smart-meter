a7f460293c512cd7dc9e0b36630ba500b7caa32a
Laurent Magnin <laurent.magnin@logimethods.com>
2017-07-05 08:49:20 -0400

    func main()  moved to the top

--- dockerfile-cassandra-inject/go/src/app/main.go
+++ dockerfile-cassandra-inject/go/src/app/main.go
@@ -25,6 +25,63 @@ const query = "INSERT INTO raw_data (" +
 // http://docs.datastax.com/en/cql/3.3/cql/cql_using/useCounters.html
 const increment = "UPDATE raw_data_count SET count = count + 1 WHERE slot = ?"
 
+func main() {
+	fmt.Println("Welcome to the NATS to Cassandra Bridge")
+
+	fmt.Println("The time is", time.Now())
+
+	nats_uri := os.Getenv("NATS_URI")
+	nc, _ := nats.Connect(nats_uri)
+
+	fmt.Println("Subscribed to NATS: ", nats_uri)
+
+	nats_subject := os.Getenv("NATS_SUBJECT")
+	fmt.Println("NATS Subject: ", nats_subject)
+
+	// CASSANDRA
+	cassandra_url := os.Getenv("CASSANDRA_URL")
+	fmt.Println("Cassandra URL: ", cassandra_url)
+
+	// LOG LEVEL
+	log_level := os.Getenv("LOG_LEVEL")
+	fmt.Println("LOG LEVEL: ", log_level)
+
+  // TASK_SLOT
+  task_slot := os.Getenv("TASK_SLOT")
+  fmt.Println("TASK SLOT: ", task_slot)
+
+	// connect to the cluster
+	cluster := gocql.NewCluster(cassandra_url)
+	cluster.Keyspace = "smartmeter"
+
+	cluster_consistency := os.Getenv("CASSANDRA_INJECT_CONSISTENCY")
+	cluster.Consistency = gocql.ParseConsistency(cluster_consistency)
+	fmt.Println("CONSISTENCY: ", cluster.Consistency)
+
+	session, _ := cluster.CreateSession()
+	defer session.Close()
+
+	fmt.Println("Connected to Cassandra")
+
+    // insert a message into Cassandra
+//    if err := session.Query(`INSERT INTO messages (subject, message) VALUES (?, ?)`,
+//        "subject1", "First message").Exec(); err != nil {
+//        log.Print(err)
+//    }
+
+	// Simple Async Subscriber
+	nc.QueueSubscribe(nats_subject, "cassandra_inject", func(m *nats.Msg) {
+		go insertIntoCassandra(session, m, task_slot, log_level)
+	})
+
+	fmt.Println("Ready to store NATS messages into CASSANDRA")
+
+	for {
+		time.Sleep(30 * time.Second)
+		// fmt.Println(time.Now())
+	}
+}
+
 func insertIntoCassandra(session *gocql.Session, m *nats.Msg, task_slot string, log_level string) {
 	/*** Point ***/
 
@@ -89,62 +146,4 @@ func insertIntoCassandra(session *gocql.Session, m *nats.Msg, task_slot string, 
         log.Print(err)
     }
   }
-
-}
-
-func main() {
-	fmt.Println("Welcome to the NATS to Cassandra Bridge")
-
-	fmt.Println("The time is", time.Now())
-
-	nats_uri := os.Getenv("NATS_URI")
-	nc, _ := nats.Connect(nats_uri)
-
-	fmt.Println("Subscribed to NATS: ", nats_uri)
-
-	nats_subject := os.Getenv("NATS_SUBJECT")
-	fmt.Println("NATS Subject: ", nats_subject)
-
-	// CASSANDRA
-	cassandra_url := os.Getenv("CASSANDRA_URL")
-	fmt.Println("Cassandra URL: ", cassandra_url)
-
-	// LOG LEVEL
-	log_level := os.Getenv("LOG_LEVEL")
-	fmt.Println("LOG LEVEL: ", log_level)
-
-  // TASK_SLOT
-  task_slot := os.Getenv("TASK_SLOT")
-  fmt.Println("TASK SLOT: ", task_slot)
-
-	// connect to the cluster
-	cluster := gocql.NewCluster(cassandra_url)
-	cluster.Keyspace = "smartmeter"
-
-	cluster_consistency := os.Getenv("CASSANDRA_INJECT_CONSISTENCY")
-	cluster.Consistency = gocql.ParseConsistency(cluster_consistency)
-	fmt.Println("CONSISTENCY: ", cluster.Consistency)
-
-	session, _ := cluster.CreateSession()
-	defer session.Close()
-
-	fmt.Println("Connected to Cassandra")
-
-    // insert a message into Cassandra
-//    if err := session.Query(`INSERT INTO messages (subject, message) VALUES (?, ?)`,
-//        "subject1", "First message").Exec(); err != nil {
-//        log.Print(err)
-//    }
-
-	// Simple Async Subscriber
-	nc.QueueSubscribe(nats_subject, "cassandra_inject", func(m *nats.Msg) {
-		go insertIntoCassandra(session, m, task_slot, log_level)
-	})
-
-	fmt.Println("Ready to store NATS messages into CASSANDRA")
-
-	for {
-		time.Sleep(30 * time.Second)
-		// fmt.Println(time.Now())
-	}
 }
