fab2bbe74bf0315a6dfddbed2b12b91acd2292b7
Laurent Magnin <laurent.magnin@logimethods.com>
2017-07-06 12:25:22 -0400

    [ci skip] build_push_remote.sh + pull

--- build_push_remote.sh
+++ build_push_remote.sh
@@ -2,32 +2,45 @@
 
 source build.sh
 
+pushd ../docker-eureka
+./build_exp.sh
+popd
+
 build_dockerfile_inject ""
 docker push logimethods/smart-meter:inject
+docker -H localhost:2374 pull logimethods/smart-meter:inject
 
 build_dockerfile_app_streaming ""
 docker push logimethods/smart-meter:app-streaming
+docker -H localhost:2374 pull logimethods/smart-meter:app-streaming
 
 build_dockerfile_app_batch ""
 docker push logimethods/smart-meter:app-batch
+docker -H localhost:2374 pull logimethods/smart-meter:app-batch
 
 build_dockerfile_prometheus ""
 docker push logimethods/smart-meter:prometheus
+docker -H localhost:2374 pull logimethods/smart-meter:prometheus
 
 build_dockerfile_monitor ""
 docker push logimethods/smart-meter:monitor
+docker -H localhost:2374 pull logimethods/smart-meter:monitor
 
 build_dockerfile_cassandra ""
 docker push logimethods/smart-meter:cassandra
+docker -H localhost:2374 pull logimethods/smart-meter:cassandra
 
 build_dockerfile_telegraf ""
 docker push logimethods/smart-meter:telegraf
+docker -H localhost:2374 pull logimethods/smart-meter:telegraf
 
 build_dockerfile_cassandra_inject ""
 docker push logimethods/smart-meter:cassandra-inject
+docker -H localhost:2374 pull logimethods/smart-meter:cassandra-inject
 
 build_dockerfile_nats_server ""
 docker push logimethods/smart-meter:nats-server
+docker -H localhost:2374 pull logimethods/smart-meter:nats-server
 
 #docker push logimethods/prometheus-nats-exporter
 #docker push logimethods/service-registry
