a4b7fbfbb6248d2aca1858b85cd442510017b41a
Laurent Magnin <laurent.magnin@logimethods.com>
2017-07-25 16:18:36 -0400

    docker pull logimethods/smart-meter:nats-client

--- pull.sh
+++ pull.sh
@@ -5,6 +5,7 @@ docker pull logimethods/smart-meter:cassandra
 docker pull logimethods/smart-meter:inject
 docker pull logimethods/smart-meter:monitor
 docker pull logimethods/smart-meter:nats-server
+docker pull logimethods/smart-meter:nats-client
 docker pull logimethods/smart-meter:app-batch
 docker pull logimethods/smart-meter:app-streaming
 docker pull logimethods/prometheus-nats-exporter
