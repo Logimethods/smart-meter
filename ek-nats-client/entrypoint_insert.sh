deb921cd93f69e5c5ff62c05390f1bd016b7e363
Laurent Magnin <laurent.magnin@logimethods.com>
2017-07-27 09:36:02 -0400

    ${NATS_USERNAME:=`cat ${NATS_USERNAME_FILE}`}

--- ek-nats-client/entrypoint_insert.sh
+++ ek-nats-client/entrypoint_insert.sh
@@ -1,5 +1,5 @@
-export NATS_USERNAME=`cat ${NATS_USERNAME_FILE}`
-export NATS_PASSWORD=`cat ${NATS_PASSWORD_FILE}`
+export NATS_USERNAME=${NATS_USERNAME:=`cat ${NATS_USERNAME_FILE}`}
+export NATS_PASSWORD=${NATS_PASSWORD:=`cat ${NATS_PASSWORD_FILE}`}
 
 env
 echo "--------------------"
