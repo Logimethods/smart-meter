0a955ef462496975ea47903997903cfbcc353ab0
Laurent Magnin <laurent.magnin@logimethods.com>
2017-08-31 13:12:46 -0400

    streaming_metrics${SECRET_MODE} ADDED

--- compose/dependencies.sh
+++ compose/dependencies.sh
@@ -18,7 +18,7 @@ inject="inject inject${SECRET_MODE} $root $cassandra"
 inject_metrics="inject_metrics $inject $metrics"
 
 streaming="streaming streaming${SECRET_MODE} $root $spark"
-streaming_metrics="streaming_metrics $streaming $metrics"
+streaming_metrics="streaming_metrics streaming_metrics${SECRET_MODE} $streaming $metrics"
 
 prediction="prediction prediction${SECRET_MODE} $root $spark $cassandra $hadoop"
 prediction_metrics="prediction_metrics $prediction $metrics"
