de1bcf168f031890cf9fec9c62f534afab5e3ca5
Laurent Magnin <laurent.magnin@logimethods.com>
2017-08-31 10:23:15 -0400

    yamlreader added to dependencies.sh

--- compose/dependencies.sh
+++ compose/dependencies.sh
@@ -1,12 +1,25 @@
-root="root"
-metrics="matrics $root"
-inject="inject $root $cassandra"
-spark="spark $root"
-streaming="streaming $root $spark"
-cassandra="cassandra $root"
+#!/bin/bash
 
-eval echo "\$$@" \
-  | sed s/["^ "]*/\&-secrets" "\&/g \
+## ./compose/dependencies.sh "_secrets" inject_metrics
+
+SECRET_MODE="$1"
+shift 1
+
+root="root"
+metrics="metrics $root"
+spark="spark $root"
+cassandra="cassandra $root"
+hadoop="hadoop $root"
+
+inject="inject inject${SECRET_MODE} $root $cassandra"
+inject_metrics="inject_metrics $inject $metrics"
+
+streaming="streaming streaming${SECRET_MODE} $root $spark"
+streaming_metrics="streaming_metrics $streaming $metrics"
+
+prediction="prediction prediction${SECRET_MODE} $root $spark $cassandra $hadoop"
+prediction_metrics="prediction_metrics $prediction $metrics"
+
+yamlreader $( eval echo "\$$@" \
   | xargs -n1 | sort -u | xargs \
-  | sed s/["^ "]*/docker-compose-\&.yml/g
-#echo "$spark"
+  | sed s/["^ "]*/compose'\/'docker-compose-\&.yml/g )
