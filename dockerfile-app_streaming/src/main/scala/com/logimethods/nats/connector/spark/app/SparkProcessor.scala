791aecf8a1c54ca34052e22c88616959bcf92ea4
Laurent Magnin <laurent.magnin@logimethods.com>
2017-11-17 09:26:24 -0500

    sc.addJar(file.getAbsolutePath) REMOVED

--- dockerfile-app_streaming/src/main/scala/com/logimethods/nats/connector/spark/app/SparkProcessor.scala
+++ dockerfile-app_streaming/src/main/scala/com/logimethods/nats/connector/spark/app/SparkProcessor.scala
@@ -37,46 +37,46 @@ trait SparkProcessor {
     val outputSubject = args(1)
 //    val outputNatsStreaming = outputSubject.toUpperCase.contains("STREAMING")
     println("Will process messages from '" + inputSubject + "' to '" + outputSubject + "'")
-    
+
     val logLevel = scala.util.Properties.envOrElse("LOG_LEVEL", "INFO")
     println("LOG_LEVEL = " + logLevel)
-    
+
     val targets = scala.util.Properties.envOrElse("TARGETS", "ALL")
     println("TARGETS = " + targets)
-  
+
     val cassandraUrl = System.getenv("CASSANDRA_URL")
     println("CASSANDRA_URL = " + cassandraUrl)
-  
+
     val sparkMasterUrl = System.getenv("SPARK_MASTER_URL")
     println("SPARK_MASTER_URL = " + sparkMasterUrl)
-  
+
     val sparkCoresMax = System.getenv("SPARK_CORES_MAX")
     println("SPARK_CORES_MAX = " + sparkCoresMax)
-    
+
     val conf = new SparkConf()
                   .setAppName(args(2))
                   .setMaster(sparkMasterUrl)
                   .set("spark.cores.max", sparkCoresMax)
                   .set("spark.cassandra.connection.host", cassandraUrl);
     val sc = new SparkContext(conf);
-    val jarFilesRegex = "(.*)jar"
-    for (file <- new File("/app/").listFiles.filter(_.getName.matches(jarFilesRegex))) 
-      { sc.addJar(file.getAbsolutePath) }
+/*    val jarFilesRegex = "(.*)jar"
+    for (file <- new File("/app/").listFiles.filter(_.getName.matches(jarFilesRegex)))
+      { sc.addJar(file.getAbsolutePath) } */
 //    val streamingDuration = scala.util.Properties.envOrElse("STREAMING_DURATION", "2000").toInt
 //    val ssc = new StreamingContext(sc, new Duration(streamingDuration));
 ///    ssc.checkpoint("/spark/storage")
-  
+
     val properties = new Properties();
     val natsUrl = System.getenv("NATS_URI")
     println("NATS_URI = " + natsUrl)
     properties.put("servers", natsUrl)
     properties.put(PROP_URL, natsUrl)
-  
+
     val clusterId = System.getenv("NATS_CLUSTER_ID")
-    
+
     val inputNatsStreaming = inputSubject.toUpperCase.contains("STREAMING")
     val outputNatsStreaming = outputSubject.toUpperCase.contains("STREAMING")
-    
+
     (properties, targets, logLevel, sc, inputNatsStreaming, inputSubject, outputSubject, clusterId, outputNatsStreaming, natsUrl)
   }
 }
@@ -85,13 +85,13 @@ trait SparkProcessor {
 trait SparkStreamingProcessor extends SparkProcessor {
   def setupStreaming(args: Array[String]) = {
     val (properties, target, logLevel, sc, inputNatsStreaming, inputSubject, outputSubject, clusterId, outputNatsStreaming, natsUrl) = setup(args)
-    
+
     val streamingDuration = scala.util.Properties.envOrElse("STREAMING_DURATION", "2000").toInt
     println("STREAMING_DURATION = " + streamingDuration)
-    
+
     val ssc = new StreamingContext(sc, new Duration(streamingDuration));
 //    ssc.checkpoint("/spark/storage")
-    
+
     (properties, target, logLevel, sc, ssc, inputNatsStreaming, inputSubject, outputSubject, clusterId, outputNatsStreaming, natsUrl, streamingDuration)
   }
 }
