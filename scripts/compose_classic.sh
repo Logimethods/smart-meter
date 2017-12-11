d53275abc4d9fefd398b64609cf066dc06ce2d59
Laurent Magnin <laurent.magnin@logimethods.com>
2017-12-11 14:40:42 -0500

    app_*

--- scripts/compose_classic.sh
+++ scripts/compose_classic.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+#docker pull logimethods/smart-meter:app_streaming-1.0-dev
+#docker pull logimethods/smart-meter:app_inject-1.0-dev
+#docker pull logimethods/smart-meter:app_compose-1.0-dev
+
 docker network create --attachable --driver overlay deetazilla
 docker stack rm deetazilla
-docker pull logimethods/smart-meter:compose-1.0-dev
 
-docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/smart-meter:compose-1.0-dev \
+docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/smart-meter:app_compose-1.0-dev \
   combine_services -p alt_properties -e "local" "single" "no_secrets" \
-  inject streaming monitor > docker-compose-merge.yml
+  inject streaming cassandra > docker-compose-merge.yml
+#  inject streaming monitor > docker-compose-merge.yml
 
 docker-compose -f docker-compose-merge.yml up
 
