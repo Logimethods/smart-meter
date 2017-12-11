e073f26b793c2f1ee591140eabc4ee8abba06cad
Laurent Magnin <laurent.magnin@logimethods.com>
2017-12-11 18:47:57 -0500

    asStreamOfKeyValue(ssc)

--- dockerfile-app_inject/src/main/scala/com/logimethods/smartmeter/generate/ValueProvider.scala
+++ dockerfile-app_inject/src/main/scala/com/logimethods/smartmeter/generate/ValueProvider.scala
@@ -13,59 +13,59 @@ import java.nio.ByteBuffer
 import math._
 import java.time.DayOfWeek._
 
-class ConsumerInterpolatedVoltageProvider(slot: Int, usersPerSec: Double, streamingDuration: Int, 
+class ConsumerInterpolatedVoltageProvider(slot: Int, usersPerSec: Double, streamingDuration: Int,
                                           randomness: Float, predictionLength: Int) extends NatsMessage {
   import java.time._
   import scala.math._
   import scala.collection.mutable.Queue
 
   val random = scala.util.Random
-  
+
   val (lineNb, transformerNb, usagePointNb) = ProviderUtil.computeNbOfElements(usersPerSec)
 
   var line = lineNb
   var transformer = transformerNb
   var usagePoint = usagePointNb + 1
-  
+
   val incr = ProviderUtil.computeIncr(streamingDuration)
   var date =  LocalDateTime.ofEpochSecond(TimeProvider.time(), 0, ZoneOffset.MIN)
   var tictac = true
-  
+
   val defaultValues: Array[Float] = Array(100.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f)
-  
+
   sealed trait ReturnType
   object ReturnType {
     case object VOLTAGE extends ReturnType;
     case object TEMPERATURE extends ReturnType;
     case object FORECAST extends ReturnType;
   }
-  
+
   var returnType: ReturnType = ReturnType.TEMPERATURE
-  
+
   var temperatures = Queue(5.0f)
   for (i <- 1 until predictionLength) {
     temperatures += (temperatures.front + (random.nextFloat() - 0.5f))
   }
-  
+
   def temperature() = {
     temperatures += TemperatureProvider.temperature(date, random.nextFloat())
     temperatures.dequeue()
   }
-  
+
   def forecast() = {
     temperatures.last
   }
-  
+
   def voltage() = {
     val baseVoltage = ConsumerInterpolatedVoltageProfile
-                          .valueAtDayAndHour(point(), 
+                          .valueAtDayAndHour(point(),
                                             date.getDayOfWeek(),
                                             date.getHour(),
                                             randomness * (random.nextFloat() - 0.5f))
-                                            
+
     baseVoltage + ((temperatures.front -5).abs * 0.08f) - 1
   }
-  
+
   def increment() {
     date = LocalDateTime.ofEpochSecond(TimeProvider.time(), 0, ZoneOffset.MIN)
     if (tictac) {
@@ -90,8 +90,8 @@ class ConsumerInterpolatedVoltageProvider(slot: Int, usersPerSec: Double, stream
     }
     tictac = ! tictac
   }
-      
-  def getSubject() = { 
+
+  def getSubject() = {
     increment()
     returnType match {
       case ReturnType.TEMPERATURE => ".temperature"
@@ -99,7 +99,7 @@ class ConsumerInterpolatedVoltageProvider(slot: Int, usersPerSec: Double, stream
       case ReturnType.VOLTAGE => ".data." + point()
     }
   }
-  
+
   def getPayload(): Array[Byte] = {
     increment()
     val value = returnType match {
@@ -107,54 +107,54 @@ class ConsumerInterpolatedVoltageProvider(slot: Int, usersPerSec: Double, stream
       case ReturnType.FORECAST => forecast()
       case ReturnType.VOLTAGE => voltage()
     }
-    
+
     usagePoint += 1
 
     return encodePayload(date, value)
   }
-  
+
   def encodePayload(date: LocalDateTime, value: Float): Array[Byte] = {
     // https://docs.oracle.com/javase/8/docs/api/java/nio/ByteBuffer.html
     val buffer = returnType match {
       case ReturnType.VOLTAGE => ByteBuffer.allocate(8+(4*12))
       case _ => ByteBuffer.allocate(8+4)
     }
-    
+
     returnType match {
       case ReturnType.FORECAST => buffer.putLong(date.plusHours(12).atOffset(ZoneOffset.MIN).toEpochSecond())
-      case _ => buffer.putLong(date.atOffset(ZoneOffset.MIN).toEpochSecond())
+      case _ => buffer.putLong(date.atOffset(ZoneOffset.UTC).toEpochSecond())
     }
-    
+
     buffer.putFloat(value)
 
     returnType match {
-      case ReturnType.VOLTAGE => 
+      case ReturnType.VOLTAGE =>
         for (i <- 0 to (defaultValues.length - 1)) { buffer.putFloat(defaultValues(i) + random.nextFloat()) }
-      case _ => 
+      case _ =>
     }
-    
-    return buffer.array()    
+
+    return buffer.array()
   }
-  
+
   def point(): String = {
     return ((10*slot) + line + "." + transformer + "." + usagePoint)
   }
 }
 
 object ProviderUtil {
-  
+
   def computeNbOfElements(usersPerSec: Double) = {
     val lineNtransformer = (math.cbrt(usersPerSec)).toInt
     val lineNb = (math.cbrt(lineNtransformer)).toInt
     val transformerNb = (lineNtransformer / lineNb).toInt
     val usagePointNb = (usersPerSec / (lineNb * transformerNb)).toInt
-    
+
     print("computeNbOfElements: ")
     println (lineNb, transformerNb, usagePointNb)
-    
+
     (lineNb, transformerNb, usagePointNb)
-  }  
-  
+  }
+
   def computeIncr(streamingDuration: Int) = {
     60000 / streamingDuration
   }
--- dockerfile-app_streaming/src/main/scala/com/logimethods/nats/connector/spark/app/SparkMaxProcessor.scala
+++ dockerfile-app_streaming/src/main/scala/com/logimethods/nats/connector/spark/app/SparkMaxProcessor.scala
@@ -49,22 +49,55 @@ object SparkMaxProcessor extends App with SparkStreamingProcessor {
         .withNatsURL(natsUrl)
         .withSubjects(inputSubject)
         .withDataDecoder(dataDecoder)
-        .asStreamOf(ssc)
+        .asStreamOfKeyValue(ssc)
     } else {
       NatsToSparkConnector
         .receiveFromNats(classOf[Tuple2[Long,Float]], StorageLevel.MEMORY_ONLY)
         .withProperties(properties)
         .withSubjects(inputSubject)
         .withDataDecoder(dataDecoder)
-        .asStreamOf(ssc)
+        .asStreamOfKeyValue(ssc)
     }
 
-  voltages.saveToCassandra("smartmeter", "raw_data")
+  // smartmeter.voltage.data.3.3.2   | (2016-11-16T20:05:04,116.366646)
+  /**
+  line tinyint,    // 8-bit signed int
+  transformer int, // 32-bit signed int
+  usagePoint int,
+  year smallint,    // 16-bit signed int
+  month tinyint,
+  day tinyint,
+  hour tinyint,
+  minute tinyint,
+  day_of_week tinyint,
+  voltage float,
+  **/
+  val raw_data = voltages.map(
+    {case (key, (epoch, voltage)) =>
+      // println(key)
+      val tokens = key.split('.').reverse
+      // println(tokens.mkString("<", ",", ">"))
+      val line = tokens(2).toInt
+      val transformer = tokens(1).toInt
+      val usagePoint = tokens(0).toInt
 
-  if (logLevel.contains("MESSAGES")) {
-    voltages.print()
-  }
+      import java.time._
+      val date = LocalDateTime.ofEpochSecond(epoch, 0, ZoneOffset.UTC)
+      val year = date.getYear
+      val month = date.getMonthValue
+      val day = date.getDayOfYear
+      val hour = date.getHour
+      val minute = date.getMinute
+      val day_of_week = date.getDayOfYear
 
+      (line, transformer, usagePoint, year, month, day, hour, minute, day_of_week, voltage.toFloat)
+    })
+  raw_data.saveToCassandra("smartmeter", "raw_data")
+
+//  if (logLevel.contains("MESSAGES")) {
+    raw_data.print()
+//  }
+/*
   val maxByEpoch = voltages.reduceByKey(Math.max(_,_))
   maxByEpoch.saveToCassandra("smartmeter", "max_voltage")
 
@@ -80,7 +113,7 @@ object SparkMaxProcessor extends App with SparkStreamingProcessor {
 
   if (logLevel.contains("MAX_REPORT")) {
     maxReport.print()
-  }
+  }*/
 
   // Start //
   ssc.start();
