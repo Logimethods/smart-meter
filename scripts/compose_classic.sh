627198d19bb5b762d46d234ee63d1bdad1c3e0bd
Laurent Magnin <laurent.magnin@logimethods.com>
2018-02-06 09:51:42 -0500

    All images pulled

--- scripts/compose_classic.sh
+++ scripts/compose_classic.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-#docker pull logimethods/smart-meter:app_streaming-1.0-dev
-#docker pull logimethods/smart-meter:app_inject-1.0-dev
-#docker pull logimethods/smart-meter:app_compose-1.0-dev
+docker pull logimethods/smart-meter:app_inject-1.0-dev
+docker pull logimethods/smart-meter:app_compose-1.0-dev
+docker pull logimethods/dz_telegraf:1.0-dev
+docker pull logimethods/smart-meter:app_telegraf-1.0-dev
+docker pull logimethods/smart-meter:app_monitor-1.0-dev
+docker pull logimethods/smart-meter:app_cassandra-1.0-dev
+docker pull logimethods/smart-meter:app_streaming-1.0-dev
+docker pull logimethods/smart-meter:app_prometheus-1.0-dev
 
-docker network create --attachable --driver overlay deetazilla
-docker stack rm deetazilla
+docker network create --attachable --driver overlay smartmeter
+docker volume create grafana-volume
+
+## docker stack rm smartmeter
 
 docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/smart-meter:app_compose-1.0-dev \
   combine_services -p alt_properties -e "local" "single" "no_secrets" \
-  inject streaming cassandra > docker-compose-merge.yml
+  inject streaming_metrics prediction_metrics > docker-compose-merge.yml
 #  inject streaming monitor > docker-compose-merge.yml
+#  root_metrics inject streaming_metrics cassandra prediction_metrics > docker-compose-merge.yml
+#  root_metrics inject streaming_metrics cassandra prediction_metrics > docker-compose-merge.yml
+#  inject streaming_metrics cassandra prediction_metrics > docker-compose-merge.yml
+#  inject_metrics streaming cassandra monitor prediction_metrics > docker-compose-merge.yml
 
 docker-compose -f docker-compose-merge.yml up
 
