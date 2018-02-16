595397f3ac1d20680f23a035c3b9efb9cb18edd6
Laurent Magnin <laurent.magnin@logimethods.com>
2018-02-16 16:04:19 -0500

    Grafana Fixed

--- scripts/compose_classic.sh
+++ scripts/compose_classic.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-docker pull logimethods/smart-meter:app_inject-1.0-dev
-docker pull logimethods/smart-meter:app_compose-1.0-dev
-docker pull logimethods/dz_telegraf:1.0-dev
-docker pull logimethods/smart-meter:app_telegraf-1.0-dev
-docker pull logimethods/smart-meter:app_monitor-1.0-dev
-docker pull logimethods/smart-meter:app_cassandra-1.0-dev
-docker pull logimethods/smart-meter:app_streaming-1.0-dev
-docker pull logimethods/smart-meter:app_prometheus-1.0-dev
+docker_tag="1.5-dev"
+
+docker pull logimethods/smart-meter:app_inject-${docker_tag}
+docker pull logimethods/smart-meter:app_compose-${docker_tag}
+docker pull logimethods/dz_telegraf:${docker_tag}
+docker pull logimethods/smart-meter:app_telegraf-${docker_tag}
+docker pull logimethods/smart-meter:app_monitor-${docker_tag}
+docker pull logimethods/smart-meter:app_cassandra-${docker_tag}
+docker pull logimethods/smart-meter:app_streaming-${docker_tag}
+docker pull logimethods/smart-meter:app_prometheus-${docker_tag}
 
 docker network create --attachable --driver overlay smartmeter
 docker volume create grafana-volume
 
 ## docker stack rm smartmeter
 
-docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/smart-meter:app_compose-1.0-dev \
+docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/smart-meter:app_compose-${docker_tag} \
   combine_services -p alt_properties -e "local" "single" "no_secrets" \
-  inject streaming_metrics prediction_metrics > docker-compose-merge.yml
+  root_metrics inject streaming_metrics prediction_metrics > docker-compose-merge.yml
 #  inject streaming monitor > docker-compose-merge.yml
 #  root_metrics inject streaming_metrics cassandra prediction_metrics > docker-compose-merge.yml
 #  root_metrics inject streaming_metrics cassandra prediction_metrics > docker-compose-merge.yml
